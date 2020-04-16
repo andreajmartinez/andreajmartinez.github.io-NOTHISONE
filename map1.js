3d9454bbe82f9ed1347890ec576ee7e46ca16671
Andrea Martinez <andreamartinez@Andreas-MacBook-Air-2.local>
2020-04-16 00:07:06 -0500

    j

--- map1.js
+++ map1.js
@@ -1,16 +1,18 @@
-var mymap = L.map('mapid').setView([30.96, -90.42], 7)
+var mymap = L.map('mapid').setView([30.96, -92.42], 7)
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(mymap)
-var marker = L.marker([29.950923, -90.081621]).addTo(mymap)
-var polygon = L.polygon([
+var SuperDome = L.marker([29.950923, -90.081621]).addTo(mymap)
+var LakePont = L.polygon([
   [30.0335, -90.148],
   [30.145, -89.700],
   [30.375, -90.208],
   [30.180, -89.873]
 ]).addTo(mymap)
-var line = L.polygon([
+SuperDome.bindPopup('Mercedes Benz Superdome Stadium.')
+LakePont.bindPopup('Lake Pontchartrain.')
+var latlngs = [
   [30.0240, -90.156],
   [30.356, -90.090]
-]).addTo(mymap)
-marker.bindPopup('Mercedes Benz Superdome Stadium.')
-polygon.bindPopup('Lake Pontchartrain.')
-line.bindPopup('lake Pontchartrain Cswy')
+]
+var LakeCwsy = L.polyline([latlngs, { color: 'red' }]).addTo(mymap)
+mymap.fitBounds(polyline.getBounds())
+LakeCwsy.bindPopup('lake Pontchartrain Cswy')
