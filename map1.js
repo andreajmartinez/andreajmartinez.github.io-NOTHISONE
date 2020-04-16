70fb5ccedf2ed201ec6ea59ad13b0922b3cf03ef
Andrea Martinez <andreamartinez@Andreas-MacBook-Air-2.local>
2020-04-15 23:39:21 -0500

    trial

--- map1.js
+++ map1.js
@@ -1,10 +1,10 @@
-var mymap = L.map('mapid').setView([47, 8], 13)
+var mymap = L.map('mapid').setView([51.5, -0.09], 13)
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(mymap)
 var marker = L.marker([47.380925, 8.537316]).addTo(mymap)
 var polygon = L.polygon([
-  [47.382086, 8.544165],
-  [47.379333, 8.538436],
-  [47.37873, 8.541666]
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
 ]).addTo(mymap)
 var line = L.polygon([
   [47.377113, 8.552502],
