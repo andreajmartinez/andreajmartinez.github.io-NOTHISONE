2b80cfcc717c16e3ec1768d572844d9db73943a5
Andrea Martinez <andreamartinez@Andreas-MacBook-Air-2.local>
2020-04-15 23:48:20 -0500

    PLS

--- map1.js
+++ map1.js
@@ -1,14 +1,16 @@
-var mymap = L.map('mapid').setView([51.5, -0.09], 13)
+var mymap = L.map('mapid').setView([29.957467, -90.074335], 8)
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(mymap)
-var marker = L.marker([47.380925, 8.537316]).addTo(mymap)
+var marker = L.marker([29.950923, -90.081621]).addTo(mymap)
 var polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
+  [30.033586, -90.148483],
+  [30.14528, -89.70079],
+  [30.375397, -90.208908],
+  [30.1809, -89.873825]
 ]).addTo(mymap)
 var line = L.polygon([
-  [47.377113, 8.552502],
-  [47.276794, 8.544949]
+  [30.024074, -90.156723],
+  [30.356539, -90.090805]
 ]).addTo(mymap)
-marker.bindPopup('Zurich Bus Station.')
-polygon.bindPopup('Park Platzspitz.')
+marker.bindPopup('Mercedes Benz Superdome Stadium.')
+polygon.bindPopup('Lake Pontchartrain.')
+line.bindPopup('lake Pontchartrain Cswy')
