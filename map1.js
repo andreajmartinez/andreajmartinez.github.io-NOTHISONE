a46268184bd23ed7d548e9a1e1920c0deac7e889
Andrea Martinez <andreamartinez@Andreas-MacBook-Air-2.local>
2020-04-15 23:59:42 -0500

    d

--- map1.js
+++ map1.js
@@ -1,15 +1,15 @@
-var mymap = L.map('mapid').setView([30.96, -90.42], 5)
+var mymap = L.map('mapid').setView([30.96, -90.42], 7)
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(mymap)
 var marker = L.marker([29.950923, -90.081621]).addTo(mymap)
 var polygon = L.polygon([
-  [30.033586, -90.148483],
-  [30.14528, -89.70079],
-  [30.375397, -90.208908],
-  [30.1809, -89.873825]
+  [30.0335, -90.148],
+  [30.145, -89.700],
+  [30.375, -90.208],
+  [30.180, -89.873]
 ]).addTo(mymap)
 var line = L.polygon([
-  [30.024074, -90.156723],
-  [30.356539, -90.090805]
+  [30.0240, -90.156],
+  [30.356, -90.090]
 ]).addTo(mymap)
 marker.bindPopup('Mercedes Benz Superdome Stadium.')
 polygon.bindPopup('Lake Pontchartrain.')
