3f72404a284cf98e57a6e3ec89bdfffea3d1fec7
Andrea Martinez <andreamartinez@Andreas-MacBook-Air-2.local>
2020-04-16 00:16:49 -0500

    m

--- map1.js
+++ map1.js
@@ -2,7 +2,7 @@ var mymap = L.map('mapid').setView([30.96, -92.42], 7)
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(mymap)
 var superdome = L.marker([29.950923, -90.081621]).addTo(mymap)
 var lakepont = L.polygon([
-  [30.033, -90.14],
+  [30.03, -90.14],
   [30.14, -89.70],
   [30.37, -90.20],
   [30.18, -89.87]
