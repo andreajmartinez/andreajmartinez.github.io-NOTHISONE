a38f526dfb57a4df65526b6cef9a9a7fde95dd66
Andrea Martinez <andreamartinez@Andreas-MacBook-Air-2.local>
2020-04-16 00:52:10 -0500

    s

--- map1.js
+++ map1.js
@@ -1,17 +1,16 @@
-var mymap = L.map('mapid').setView([31.69, -100], 5)
+var mymap = L.map('mapid').setView([31.69, -100], 7)
 L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(mymap)
 var houston = L.marker([29.801951, -95.355999]).addTo(mymap)
 var southtexas = L.polygon([
   [36.491, -103.046],
   [33.504, -94.125],
-  [29.916, -93.510],
-  [25.996, -97.377]
+  [29.916, -93.510]
 ]).addTo(mymap)
 houston.bindPopup('City of Houston.')
 southtexas.bindPopup('Area of Southern Texas.')
 var latlngs = [
   [36.491, -103.046],
-  [33.504, -94.125]
+  [25.996, -97.377]
 ]
 var polyline = L.polyline([latlngs, { color: 'red' }]).addTo(mymap)
 mymap.fitBounds(polyline.getBounds())
